ec3c42584d5ea32202b984a5f85fea1b3b3ebe35
董豪 <1097975417@qq.com>
2016-06-17 17:53:19 +0800

    fix getGeohash (#3)

--- src/getGeohash.js
+++ src/getGeohash.js
@@ -1,30 +1,32 @@
-const getGeohash = new Promise((resolve, reject) => {
-  const getGeohashFromApp = () => {
-    if (!window.hybridAPI) {
-      console.warn('please make sure the hybridAPI is existing when get geohash from app.')
-      return reject('hybridAPI is required.')
-    }
+const getGeohash = () => {
+  return new Promise((resolve, reject) => {
+      const getGeohashFromApp = () => {
+        if (!window.hybridAPI) {
+          console.warn('please make sure the hybridAPI is existing when get geohash from app.')
+          return reject('hybridAPI is required.')
+        }
 
-    let timeout = setTimeout(() => {
-      reject('can\'t get geohash in any case.')
-    }, 5000);
+        let timeout = setTimeout(() => {
+          reject('can\'t get geohash in any case.')
+        }, 5000);
 
-    hybridAPI.getGlobalGeohash(geohash => {
-      clearTimeout(timeout)
-      resolve(geohash)
+        hybridAPI.getGlobalGeohash(geohash => {
+          clearTimeout(timeout)
+          resolve(geohash)
+        });
+      };
+
+      const UrlToken = window.UParams && new UParams(location.href) || {}
+      if (UrlToken.geohash) {
+        try {
+          resolve(UrlToken.geohash)
+        } catch(error) {
+          getGeohashFromApp()
+        }
+      } else {
+        getGeohashFromApp()
+      }
     });
-  };
-
-  const UrlToken = window.UParams && new UParams(location.href) || {}
-  if (UrlToken.geohash) {
-    try {
-      resolve(UrlToken.geohash)
-    } catch(error) {
-      getGeohashFromApp()
-    }
-  } else {
-    getGeohashFromApp()
-  }
-});
+}
 
 export default getGeohash
